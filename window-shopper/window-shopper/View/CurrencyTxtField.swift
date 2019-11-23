98232d358afb6043644ba9041735a4da611f35e6
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2019-11-23 18:47:26 -0500

    Fields and keyboard accessory

---
+++ window-shopper/window-shopper/View/CurrencyTxtField.swift
@@ -0,0 +1,36 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Kilyan SOCKALINGUM on 23/11/2019.
+//  Copyright © 2019 Kilyan SOCKALINGUM. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CurrencyTxtField: UITextField {
+
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+    
+    
+    func customizeView(){
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
