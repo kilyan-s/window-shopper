4d36b942658f736deecc51ad5aa607eef553e152
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2019-11-23 17:55:33 -0500

    Create Xcode project

---
+++ window-shopper/window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Kilyan SOCKALINGUM on 23/11/2019.
+//  Copyright © 2019 Kilyan SOCKALINGUM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
